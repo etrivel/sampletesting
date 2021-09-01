432f8d74682c96e54135c5065308fb704f75e902
etrivel <vetrirudra@gmail.com>
2021-09-01 13:10:29 +0530

    projectangular

---
+++ src/app/sports/sports.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sports',
+  template: `
+    <p>
+      sports works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SportsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
